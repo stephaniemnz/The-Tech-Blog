b16fc900c88646b9fd353bb4628cd7c94924c970
stephramos <stephaniemnz@gmail.com>
2024-04-12 23:51:11 -0500

    added Main Public folder

--- Develop/controllers/api/commentsRoutes.js
+++ Develop/controllers/api/commentsRoutes.js
@@ -7,9 +7,12 @@ router.post('/', async (req, res) => {
         const commentData = await Comments.create({
             user_id: req.session.user_id,
             body: req.body.body,
+            created_at: new Date(),
         });
         res.status(200).json(commentData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+module.exports = router;
